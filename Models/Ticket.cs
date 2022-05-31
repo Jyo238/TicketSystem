51c41920d89acbc4b09f6d42c95d001bb489c8cf
Johnny <Johnny.gu.tcg@gmail.com>
2022-05-31 20:15:31 +0800

    ticket model

---
+++ Models/Ticket.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TicketSystem.Enums;
+
+namespace TicketSystem.Models
+{
+    public class Ticket
+    {
+        public int TicketId { get; set; }
+        public string Title { get; set; }
+        public TicketType Type { get; set; }
+        public TicketStatus Status { get; set; }
+        [Required]
+        public string Summary { get; set; }
+        [Required]
+        public string Description { get; set; }
+    }
+}
