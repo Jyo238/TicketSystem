30534e528aee41b0780f698b20064a952c7906a0
Johnny <Johnny.gu.tcg@gmail.com>
2022-06-02 15:22:56 +0800

    migrations

---
+++ Data/Migrations/20220531122127_userID_Status.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TicketSystem.Data.Migrations
+{
+    public partial class userID_Status : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerID",
+                table: "Ticket",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OwnerID",
+                table: "Ticket");
+        }
+    }
+}
---
+++ Data/Migrations/20220602070441_migration-20220602.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TicketSystem.Data.Migrations
+{
+    public partial class migration20220602 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AuthStatus",
+                table: "Ticket",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AuthStatus",
+                table: "Ticket");
+        }
+    }
+}
