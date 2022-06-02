5408948b52a4facefeb4bf537fe604d5e395e027
Johnny <Johnny.gu.tcg@gmail.com>
2022-06-02 15:24:08 +0800

    seedData

--- Data/SeedData.cs
+++ Data/SeedData.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TicketSystem.Authorization;
+using TicketSystem.Enums;
 using TicketSystem.Models;
 
 namespace TicketSystem.Data
@@ -12,11 +15,79 @@ namespace TicketSystem.Data
     {
         public static async Task Initialize(IServiceProvider serviceProvider, string testUserPw)
         {
-            using (var context = new ApplicationDbContext(
-                serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+            using var context = new ApplicationDbContext(serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>());
+            // For sample purposes seed both with the same password.
+            // Password is set with the following:
+            // dotnet user-secrets set SeedUserPW <pw>
+            // The admin user can do anything
+
+            var adminID = await EnsureUser(serviceProvider, testUserPw, "admin@contoso.com");
+            await EnsureRole(serviceProvider, adminID, Constants.TicketAdministratorsRole);
+
+            // allowed user can create and edit contacts that they create
+            var managerID = await EnsureUser(serviceProvider, testUserPw, "manager@contoso.com");
+            await EnsureRole(serviceProvider, managerID, Constants.TicketManagersRole);
+
+            SeedDB(context, adminID);
+        }
+
+        private static async Task<string> EnsureUser(IServiceProvider serviceProvider,
+                                            string testUserPw, string UserName)
+        {
+            var userManager = serviceProvider.GetService<UserManager<IdentityUser>>();
+
+            var user = await userManager.FindByNameAsync(UserName);
+            if (user == null)
             {
-                SeedDB(context, "0");
+                user = new IdentityUser
+                {
+                    UserName = UserName,
+                    EmailConfirmed = true
+                };
+                await userManager.CreateAsync(user, testUserPw);
             }
+
+            if (user == null)
+            {
+                throw new Exception("The password is probably not strong enough!");
+            }
+
+            return user.Id;
+        }
+
+        private static async Task<IdentityResult> EnsureRole(IServiceProvider serviceProvider,
+                                                                      string uid, string role)
+        {
+            var roleManager = serviceProvider.GetService<RoleManager<IdentityRole>>();
+
+            if (roleManager == null)
+            {
+                throw new Exception("roleManager null");
+            }
+
+            IdentityResult IR;
+            if (!await roleManager.RoleExistsAsync(role))
+            {
+                IR = await roleManager.CreateAsync(new IdentityRole(role));
+            }
+
+            var userManager = serviceProvider.GetService<UserManager<IdentityUser>>();
+
+            //if (userManager == null)
+            //{
+            //    throw new Exception("userManager is null");
+            //}
+
+            var user = await userManager.FindByIdAsync(uid);
+
+            if (user == null)
+            {
+                throw new Exception("The testUserPw password was probably not strong enough!");
+            }
+
+            IR = await userManager.AddToRoleAsync(user, role);
+
+            return IR;
         }
 
         public static void SeedDB(ApplicationDbContext context, string adminID)
@@ -32,6 +103,8 @@ namespace TicketSystem.Data
                     Title = "Bug one",
                     Summary = "1234",
                     Description = "好多bug",
+                    AuthStatus = AuthorizationStatus.Approved,
+                    OwnerID = adminID
                 },
                new Ticket
                {
