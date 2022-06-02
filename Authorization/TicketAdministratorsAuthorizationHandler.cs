76ce017bdfb5e83a0ff3cb92fc249394438bf6e7
Johnny <Johnny.gu.tcg@gmail.com>
2022-06-02 15:23:10 +0800

    set authorizations

---
+++ Authorization/TicketAdministratorsAuthorizationHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using TicketSystem.Authorization;
+using TicketSystem.Models;
+
+namespace TicketManager.Authorization
+{
+    public class TicketAdministratorsAuthorizationHandler
+                    : AuthorizationHandler<OperationAuthorizationRequirement, Ticket>
+    {
+        protected override Task HandleRequirementAsync(
+                                              AuthorizationHandlerContext context,
+                                    OperationAuthorizationRequirement requirement,
+                                     Ticket resource)
+        {
+            if (context.User == null)
+            {
+                return Task.CompletedTask;
+            }
+
+            // Administrators can do anything.
+            if (context.User.IsInRole(Constants.TicketAdministratorsRole))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
