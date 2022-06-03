7600c1d703b2f8b4ab56af02e5cbda25060ffb69
johnny.kuo <johnny.gu.tcg@gmail.com>
2022-06-04 01:16:45 +0800

    remove AuthStatus

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -10,12 +10,11 @@ namespace TicketSystem.Models
         // user ID from AspNetUser table.
         public string OwnerID { get; set; }
         public TicketType Type { get; set; }
-        public TicketStatus Status { get; set; }
         [Required]
         public string Summary { get; set; }
         [Required]
         public string Description { get; set; }
 
-        public AuthorizationStatus AuthStatus { get; set; }
+        public AuthorizationStatus Status { get; set; }
     }
 }
