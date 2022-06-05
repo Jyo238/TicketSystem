d7586e4f8bcfc53d4a862878d4f20a14093f199e
johnny.kuo <johnny.gu.tcg@gmail.com>
2022-06-05 18:23:50 +0800

    fix some connect problem

---
+++ Data/Migrations/20220605100804_20220605DB.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TicketSystem.Data.Migrations
+{
+    public partial class _20220605DB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AuthStatus",
+                table: "Ticket");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Ticket");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AuthStatus",
+                table: "Ticket",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Ticket",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
